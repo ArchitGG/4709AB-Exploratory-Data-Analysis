d13f5a7209beba9d84fc5c60c3f50652b85cf7b8
Archit Gubiligari <architgg@uw.edu>
2022-08-03 16:17:26 -0700

    Update Table of Summary Information.R

--- Table of Summary Information.R
+++ Table of Summary Information.R
@@ -3,20 +3,13 @@ CovidData <- read_csv("C:/Users/ArchitGG/Desktop/us_state_vaccinations.csv")
 View(CovidData)
 
 library(dplyr)
+library(tidyselect)
 
-na.omit(
-CovidData %>%
-rename(Location = "location") %>%
-group_by(Location) %>%
-summarize(total_vaccinations, people_vaccinated, people_fully_vaccinated, daily_vaccinations, total_boosters))
+na.omit(summary_table <- CovidData %>%
+  rename(Location = location) %>%
+  group_by(Location) %>%
+  summarize(total_vaccinations, people_vaccinated, people_fully_vaccinated, daily_vaccinations, total_boosters))
 
-data_round <- CovidData %>%
-  mutate_if(is.numeric,
-            round,
-            digits = 5)
-View(data_round2)
 
-table_1 <- table(CovidData$total_vaccinations, CovidData$people_fully_vaccinated)
 
-table(CovidData$total_vaccinations, CovidData$people_fully_vaccinated)
 
